87d0c12059996e134c074cb05c7faa07beb9ca2e
Masha <107428174+Masha2022@users.noreply.github.com>
2022-10-04 16:37:11 +0300

    Update RecoloringBehaviour.cs

--- Assets/Scripts/RecoloringBehaviour.cs
+++ Assets/Scripts/RecoloringBehaviour.cs
@@ -12,7 +12,7 @@ public class RecoloringBehaviour : MonoBehaviour
     private Color _startColor;
     private Color _nextColor;
     private Renderer _renderer;
-   // [SerializeField] private float _recoloringTime;
+    [SerializeField] private float _recoloringTime;
     [SerializeField] private float _sleepTime;
     private float _sleepTimeProgress;
 
@@ -24,26 +24,37 @@ public class RecoloringBehaviour : MonoBehaviour
 
     private void GenerateNextColor()
     {
-        var color = Random.ColorHSV(0f, 1f, 0.8f, 1f, 1f, 1f);
-        GetComponent<Renderer>().material.color = color;
-        _startColor = _renderer.material.color;
-        _nextColor = Random.ColorHSV(0f, 1f, 0.8f, 1f, 1f, 1f);
+        // Задаю рандомное значение цвета  _startColor 
+        _startColor = Random.ColorHSV(0f, 1f, 0.7f, 1f, 1f, 1f);
+        //присваиваю цвет _startColor материалу кубика
+        GetComponent<Renderer>().material.color = _startColor;
     }
 
     private void Update()
     {
-        //var progress = _recoloringTime / _recoloringDuration;
-        //var currentColor = Color.Lerp(_startColor, _nextColor, progress);
-     
+        //_sleepTimeProgress-счетчик для задержки смены цвета
+        //если счетчик для задержки смены цвета меньше чем значение задержки
         if (_sleepTimeProgress < _sleepTime)
         {
-            _sleepTimeProgress += Time.deltaTime;
-          
-            if (_sleepTimeProgress >= _sleepTime)
-            {
-                _sleepTimeProgress = 0f;
-                GenerateNextColor();
-            }
+            _sleepTimeProgress += Time.deltaTime; //тогда добавляю время в счетчик
+            return;
+        }
+
+
+        if (_recoloringTime < _recoloringDuration) //если время для смены цвета не вышло
+        {
+            //интерполирую цвет
+            _recoloringTime += Time.deltaTime;//накапливаю время для смены цвета
+            var progress = _recoloringTime / _recoloringDuration; // делю счетчик времени для смены цвета кубика на продолжительсть смены цвета
+            var currentColor = Color.Lerp(_startColor, _nextColor, progress); //получаю текущий цвет
+            //записываю текущий цвет в следущий цвет
+            _nextColor = currentColor;
+        }
+        else
+        {
+            _recoloringTime = 0f; //иначе меняю цвет и сбрасываю счетчик в ноль.
+            _sleepTimeProgress = 0f;
+            GenerateNextColor();
         }
     }
 }
